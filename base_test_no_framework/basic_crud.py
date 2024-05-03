482201a2df6c98c0775b12a7fc020da8f6a7a740
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-05-03 02:43:33 -0600

    progress

--- base_test_no_framework/basic_crud.py
+++ base_test_no_framework/basic_crud.py
@@ -1,9 +1,11 @@
 # API Request - HTTP Request
 
+# Import necessary modules for API testing and reporting
 import allure
 import pytest
 import requests
 
+# Test case to verify successful creation of a booking
 
 @allure.title("Create Booking CRUD")
 @allure.description("TC#1 - Verify create Booking")
@@ -16,9 +18,15 @@ def test_create_booking_positive():
     # Headers
     # Payload / Data / Body
     # Auth
+
+    # API base URL and endpoint for booking creation
     base_url = "https://restful-booker.herokuapp.com"
     base_path = "/booking"
+
+    # Full URL for the API endpoint
     URL = base_url + base_path
+
+    # Define request headers and payload for the booking request
     headers = {"Content-Type": "application/json"}
     payload = {
         "firstname": "Amit",
@@ -31,6 +39,8 @@ def test_create_booking_positive():
         },
         "additionalneeds": "Breakfast"
     }
+
+    # Send a POST request to create a new booking
     response = requests.post(url=URL, headers=headers, json=payload)
     
     # Response Body  Verification,
@@ -39,19 +49,26 @@ def test_create_booking_positive():
     # JSON Schema Validation
     # Time Response
     
+    # Expect a successful response
     assert response.status_code == 200
+    # Parse the JSON response
     print(response.headers)
     response_json = response.json()
+
+    # Extract and verify booking information from the response
     booking_id = response_json["bookingid"]
     assert booking_id is not None
     assert booking_id > 0
     assert type(booking_id) == int
+
+    # Verify specific details in the booking response
     firstname = response_json["booking"]["firstname"]
     checkin = response_json["booking"]["bookingdates"]["checkin"]
     assert firstname == "Amit"
     assert checkin == "2018-01-01"
     
-    
+
+# Test case to verify that booking creation fails with invalid (empty) data    
 @allure.title("Create Booking CRUD")
 @allure.description("TC#2 - Verify Booking is not created with {} data ")
 @pytest.mark.crud
@@ -60,12 +77,18 @@ def test_create_booking_negative():
     base_url = "https://restful-booker.herokuapp.com"
     base_path = "/booking"
     URL = base_url + base_path
+
+    # Define headers and an empty payload for the request
     headers = {"Content-Type": "application/json"}
     json_payload = {}
+
+    # Send a POST request with an empty payload
     response = requests.post(url=URL, headers=headers, json=json_payload)
+
+    # Output the type of certain variables (debugging assistance)
     print(type(URL))
     print(type(headers))
     print(type(json_payload))
     
-    # Assertions
+    # Assertions, verify that the response status code indicates a failure, expect an error response due to empty data
     assert response.status_code == 500
